9b4b8463baaea825e679d14cb9cf1d52082d2cef
Michal Migurski <migurski@Philippe.local>
2010-02-25 18:35:40 -0800

    Copied the working command over from run.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 
 PORT=9955
 IAMHERE=`dirname $0`
-java -cp ${IAMHERE}/lib/core.jar:${IAMHERE}/lib/javase.jar:${IAMHERE}/lib/openjdk_071012-httpserver.jar:${IAMHERE}/lib/barcode.jar info.aaronland.barcode.WsDecode ${PORT}
+java -cp ${IAMHERE}/lib/core.jar:${IAMHERE}/lib/javase.jar:${IAMHERE}/lib/openjdk_071012-httpserver.jar:${IAMHERE}/build/dist/lib/barcode.jar info.aaronland.barcode.WsDecode ${PORT}
